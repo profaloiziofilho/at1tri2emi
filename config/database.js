526234d61241a9a69b8a8654160aef90131f38b7
profaloiziofilho <89751909+profaloiziofilho@users.noreply.github.com>
2025-06-16 08:59:37 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const conexao = async() => {
     //conexao local
     //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
     //conexao atlas
-    var atlas = await mongoose.connect('mongodb+srv://userProf:prof1234@cluster0.nwf8p.mongodb.net/ac1tri')
+    var atlas = await mongoose.connect('mongodb+srv://[user]:[password]@cluster')
 }
 
 module.exports = conexao
